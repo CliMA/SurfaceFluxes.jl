6398ce18c46e070dfa80e2d2649366b42002044d
Akshay Sridhar <asridhar@caltech.edu>
2021-11-01 14:06:51 -0700

    modified:   src/SurfaceFluxes.jl 	modified:   test/test_profiles.jl

--- src/SurfaceFluxes.jl
+++ src/SurfaceFluxes.jl
@@ -45,8 +45,8 @@ abstract type SurfaceFluxesModel end
 struct FVScheme end
 struct DGScheme end
 
-export surface_conditions, test_surface_conditions,
-    exchange_coefficients, recover_profile, monin_obukhov_length
+export surface_conditions,
+    exchange_coefficients, recover_profile, monin_obukhov_length, monin_obukhov_length_from_b
 
 """
     SurfaceFluxConditions{FT}
@@ -59,7 +59,7 @@ $(DSE.FIELDS)
 """
 struct SurfaceFluxConditions{FT, VFT}
     L_MO::FT
-    wθ_flux_star::FT
+    wb_flux_star::FT
     flux::VFT
     x_star::VFT
     C_exchange::VFT
@@ -68,7 +68,7 @@ end
 function Base.show(io::IO, sfc::SurfaceFluxConditions)
     println(io, "----------------------- SurfaceFluxConditions")
     println(io, "L_MO           = ", sfc.L_MO)
-    println(io, "wθ_flux_star = ", sfc.wθ_flux_star)
+    println(io, "wb_flux_star = ", sfc.wb_flux_star)
     println(io, "flux           = ", sfc.flux)
     println(io, "x_star         = ", sfc.x_star)
     println(io, "C_exchange     = ", sfc.C_exchange)
@@ -77,7 +77,7 @@ end
 
 function surface_fluxes_f!(F, x, nt)
     param_set = nt.param_set
-    wθ_flux_star = nt.wθ_flux_star
+    wb_flux_star = nt.wb_flux_star
     z_in = nt.z_in
     z_0 = length(nt.z_0) > 1 ? Tuple(nt.z_0) : nt.z_0
     x_in = Tuple(nt.x_in)
@@ -85,71 +85,17 @@ function surface_fluxes_f!(F, x, nt)
     n_vars = nt.n_vars
     scheme = nt.scheme
     universal_func = nt.universal_func
-    θ_scale = nt.θ_scale
     qt_scale = nt.qt_scale
 
     x_tup = Tuple(x)
 
-    u_star, θ_star, qt_star = x_tup[2], x_tup[3], x_tup[4]
-    if wθ_flux_star == nothing
-        wθ_surf_flux = -u_star * θ_star
+    u_star, b_star, qt_star = x_tup[2], x_tup[3], x_tup[4]
+    if wb_flux_star == nothing
+        wb_surf_flux = -u_star * b_star
     else
-        wθ_surf_flux = wθ_flux_star
+        wb_surf_flux = -u_star * b_star
     end
-    L_MO = monin_obukhov_length(param_set, u_star, θ_star, θ_scale, qt_scale, qt_star)
-    b_star = b_star_from_θ(param_set, u_star, θ_star, θ_scale, qt_scale, qt_star)
-
-    uf = universal_func(param_set, L_MO)
-    F_nt = ntuple(Val(n_vars + 1)) do i
-        if i == 1
-            F_i =
-                x_tup[1] - monin_obukhov_length_from_b(
-                    param_set,
-                    u_star,
-                    b_star
-                )
-        else
-            ϕ = x_tup[i]
-            transport = i - 1 == 1 ? UF.MomentumTransport() : UF.HeatTransport()
-            F_i =
-                ϕ - compute_physical_scale(
-                    uf,
-                    z_in,
-                    length(nt.z_0) > 1 ? z_0[i - 1] : z_0,
-                    x_in[i - 1],
-                    x_s[i - 1],
-                    transport,
-                    scheme,
-                )
-        end
-        F_i
-    end
-    F .= F_nt
-end
-
-function test_surface_fluxes_f!(F, x, nt)
-    param_set = nt.param_set
-    wθ_flux_star = nt.wθ_flux_star
-    z_in = nt.z_in
-    z_0 = length(nt.z_0) > 1 ? Tuple(nt.z_0) : nt.z_0
-    x_in = Tuple(nt.x_in)
-    x_s = Tuple(nt.x_s)
-    n_vars = nt.n_vars
-    scheme = nt.scheme
-    universal_func = nt.universal_func
-    θ_scale = nt.θ_scale
-    qt_scale = nt.qt_scale
-
-    x_tup = Tuple(x)
-
-    u_star, θ_star, qt_star = x_tup[2], x_tup[3], x_tup[4]
-    if wθ_flux_star == nothing
-        wθ_surf_flux = -u_star * θ_star
-    else
-        wθ_surf_flux = wθ_flux_star
-    end
-    L_MO = monin_obukhov_length(param_set, u_star, θ_scale, qt_scale, wθ_surf_flux, qt_star)
-    b_star = b_star_from_θ(param_set, u_star, θ_star, θ_scale, qt_scale, qt_star)
+    L_MO = monin_obukhov_length_from_b(param_set, u_star, b_star)
 
     uf = universal_func(param_set, L_MO)
     F_nt = x_tup[1] - monin_obukhov_length_from_b(param_set,
@@ -182,24 +128,6 @@ function test_surface_fluxes_f!(F, x, nt)
     F .= F_nt
 end
 
-"""
-    surface_conditions
-Surface conditions given
- - `MO_param_guess` initial guess for solution (`L_MO, u_star, θ_star, ϕ_star, ...`)
- - `x_in` Inner values for state variable array `x`. They correspond to volume averages in FV and
-    nodal point values in DG
- - `x_s` surface values for state variable array `x`
- - `z_0` roughness lengths for state variable array `x`
- - `vdse_scale` virtual dry static energy (i.e., basic potential temperature)
- - `moisture_scale` total moisture scale
- - `z_in` Input height for the similarity functions. It is Δz for FV and the height
-    of the first nodal point for DG
- - `wθ_flux_star` potential temperature flux (optional)
- - `universal_func` family of flux-gradient universal functions used (optional)
-
-If `wθ_flux_star` is not given, then it is computed by iteration
-of equations 3, 17, and 18 in Nishizawa2018.
-"""
 function surface_conditions(
     param_set::APS,
     MO_param_guess::AbstractVector,
@@ -210,7 +138,7 @@ function surface_conditions(
     qt_scale::FT,
     z_in::FT,
     scheme,
-    wθ_flux_star::Union{Nothing, FT} = nothing,
+    wb_flux_star::Union{Nothing, FT} = nothing,
     universal_func::Union{Nothing, F} = UF.Businger,
     sol_type::NS.SolutionType = NS.CompactSolution(),
     tol::NS.AbstractTolerance = NS.ResidualTolerance{FT}(sqrt(eps(FT))),
@@ -225,7 +153,7 @@ function surface_conditions(
 
     args = (;
         param_set,
-        wθ_flux_star,
+        wb_flux_star,
         z_in,
         z_0,
         n_vars,
@@ -247,16 +175,16 @@ function surface_conditions(
     root_tup = Tuple(sol.root)
     if sol.converged
         L_MO, x_star = root_tup[1], SA.SVector(root_tup[2:end])
-        u_star, θ_star = x_star[1], x_star[2]
+        u_star, b_star = x_star[1], x_star[2]
     else
         KA.@print("Warning: Unconverged Surface Fluxes\n")
         L_MO, x_star = root_tup[1], SA.SVector(root_tup[2:end])
-        u_star, θ_star = x_star[1], x_star[2]
+        u_star, b_star = x_star[1], x_star[2]
     end
 
     grav::FT = CPP.grav(param_set)
     von_karman_const::FT = CPSGS.von_karman_const(param_set)
-    wθ_flux_star = u_star * θ_star
+    wb_flux_star = u_star * b_star
     flux = -u_star * x_star
 
     C_exchange = get_flux_coefficients(
@@ -273,87 +201,7 @@ function surface_conditions(
     VFT = typeof(flux)
     return SurfaceFluxConditions{FT, VFT}(
         L_MO,
-        wθ_flux_star,
-        flux,
-        x_star,
-        C_exchange,
-    )
-end
-
-function test_surface_conditions(
-    param_set::APS,
-    MO_param_guess::AbstractVector,
-    x_in::AbstractVector,
-    x_s::AbstractVector,
-    z_0::Union{AbstractVector, FT},
-    θ_scale::FT,
-    qt_scale::FT,
-    z_in::FT,
-    scheme,
-    wθ_flux_star::Union{Nothing, FT} = nothing,
-    universal_func::Union{Nothing, F} = UF.Businger,
-    sol_type::NS.SolutionType = NS.CompactSolution(),
-    tol::NS.AbstractTolerance = NS.ResidualTolerance{FT}(sqrt(eps(FT))),
-    maxiter::Int = 10_000,
-) where {FT <: AbstractFloat, APS, F}
-
-    n_vars = length(MO_param_guess) - 1
-    @assert length(MO_param_guess) == n_vars + 1
-    @assert length(x_in) == n_vars
-    @assert length(x_s) == n_vars
-    local sol
-
-    args = (;
-        param_set,
-        wθ_flux_star,
-        z_in,
-        z_0,
-        n_vars,
-        x_in,
-        x_s,
-        scheme,
-        θ_scale,
-        qt_scale,
-        MO_param_guess,
-        universal_func,
-    )
-
-    # Define closure over args
-    f!(F, x_all) = test_surface_fluxes_f!(F, x_all, args)
-
-    nls = NS.NewtonsMethodAD(f!, MO_param_guess)
-    sol = NS.solve!(nls, sol_type, tol, maxiter)
-
-    root_tup = Tuple(sol.root)
-    if sol.converged
-        L_MO, x_star = root_tup[1], SA.SVector(root_tup[2:end])
-        u_star, θ_star = x_star[1], x_star[2]
-    else
-        KA.@print("Warning: Unconverged Surface Fluxes\n")
-        L_MO, x_star = root_tup[1], SA.SVector(root_tup[2:end])
-        u_star, θ_star = x_star[1], x_star[2]
-    end
-
-    grav::FT = CPP.grav(param_set)
-    von_karman_const::FT = CPSGS.von_karman_const(param_set)
-    wθ_flux_star = u_star * θ_star
-    flux = -u_star * x_star
-
-    C_exchange = get_flux_coefficients(
-        param_set,
-        z_in,
-        x_star,
-        x_s,
-        L_MO,
-        z_0,
-        scheme,
-        universal_func,
-    )
-
-    VFT = typeof(flux)
-    return SurfaceFluxConditions{FT, VFT}(
-        L_MO,
-        wθ_flux_star,
+        wb_flux_star,
         flux,
         x_star,
         C_exchange,
@@ -390,16 +238,18 @@ Returns the potential temperature and q_tot fluxes needed to
 compute the energy flux.
 """
 function get_energy_flux(surf_conds::SurfaceFluxConditions)
-    θ_flux = surf_conds.flux[2]
+    b_flux = surf_conds.flux[2]
     q_tot_flux = surf_conds.flux[3]
-    return θ_flux, q_tot_flux
+    return b_flux, q_tot_flux
 end
 
 """
     compute_physical_scale(uf, z_in, z_0, x_in, x_s, transport, ::FVScheme)
 
-Returns u_star, θ_star, ... given equations (17), (18) for FV and (8), (9) for DG
-from Nishizawa & Kitamura (2018).
+Returns u_star, b_star, ... given equations (17), (18) for FV and (8), (9) for DG
+from Nishizawa & Kitamura (2018). Consistent with computation of fluxes using 
+buoyancy scale in FMS GCM code. Allows extension to multiple tracers by including
+their influence on the buoyancy scale parameter.
 
 ## Arguments
  - `uf` universal function family
@@ -649,7 +499,7 @@ function monin_obukhov_length(param_set::APS,
     return u_star^2 / (von_karman_const * b_star + eps(FT))
 end
 """
-    monin_obukhov_length_from_b(param_set, u_star, θ_scale, qt_scale, wθ_surf_flux, qt_star)
+    monin_obukhov_length_from_b(param_set, u_star, b_star)
 
 Returns the Monin-Obukhov length as a function of b_star
 """
@@ -700,7 +550,6 @@ end
         z,
         F_exchange,
         x_star::VFT,
-        θ_scale,
         L_MO,
         universal_func,
     )
@@ -713,7 +562,6 @@ function exchange_coefficients(
     z,
     F_exchange,
     x_star::VFT,
-    θ_scale,
     L_MO,
     universal_func,
 ) where {VFT}
@@ -740,7 +588,6 @@ end
         param_set,
         z,
         x_star::VFT,
-        θ_scale,
         L_MO,
         z0,
         universal_func,
@@ -778,7 +625,7 @@ function get_flux_coefficients(
     C = similar(x_star)
     C .= ntuple(Val(length(x_star))) do i
         if i == 1
-            C_i = x_star[i]^2 / (u_in - x_s_tup[i])^2
+            C_i = x_star[i]^2 / (u_in - x_s_tup[i])^2 # Eq. (36) Byun(1990)
         else
             ϕ_in = recover_profile(
                 param_set,
@@ -793,7 +640,7 @@ function get_flux_coefficients(
             )
             C_i =
                 x_star[1] * x_star[i] / (u_in - x_s_tup[1]) /
-                (ϕ_in - x_s_tup[i])
+                (ϕ_in - x_s_tup[i]) # Eq. (37) Byun(1990)
         end
         C_i
     end
--- test/test_profiles.jl
+++ test/test_profiles.jl
@@ -1,18 +1,98 @@
 using NCDatasets
 using Plots
 using LaTeXStrings
+using SurfaceFluxes
+const SF = SurfaceFluxes
+using Statistics
 
-data = NCDataset("DYCOMS_RF01.nc");
-z = data["z_half"];
-ρ = data.group["profiles"]["rho"];
-u = data.group["profiles"]["u_mean"];
-v = data.group["profiles"]["v_mean"];
-qt = data.group["profiles"]["qt_mean"];
-ql = data.group["profiles"]["ql_mean"];
-p0 = data.group["profiles"]["p0"];
-b = data.group["profiles"]["buoyancy_mean"];
-θli = data.group["profiles"]["thetali_mean"];
+using CLIMAParameters: AbstractEarthParameterSet
+struct EarthParameterSet <: AbstractEarthParameterSet end
+const param_set = EarthParameterSet()
 
-function mean(X)
-    return mean(X, dims=2)
+files = ["DYCOMS_RF01.nc", "Bomex.nc", "Rico.nc", "Gabls.nc"];
+for (n, f) in enumerate(files)
+
+    @info "Casename = $f"
+    data = NCDataset("./test/"*f);
+    z = data["z_half"];
+    ρ = data.group["profiles"]["rho"];
+    u = data.group["profiles"]["u_mean"];
+    v = data.group["profiles"]["v_mean"];
+    qt = data.group["profiles"]["qt_mean"];
+    ql = data.group["profiles"]["ql_mean"];
+    p0 = data.group["profiles"]["p0"];
+    b = data.group["profiles"]["buoyancy_mean"];
+    θli = data.group["profiles"]["thetali_mean"];
+
+    function ave(X)
+        return mean(X, dims=2)
+    end
+
+    # Data at first interior node (x_ave)
+    ii = 2
+    FT = Float32
+    ArrayType = Array
+
+    z_ave = Tuple(z)[ii]
+    b_ave = Tuple(ave(b))[ii]
+    θ_ave = Tuple(ave(θli))[ii]
+    u_ave = Tuple(ave(u))[ii]
+    qt_ave = Tuple(ave(qt))[ii]
+    x_ave = ArrayType(FT[u_ave, b_ave, qt_ave])
+
+    ## Initial guesses for MO parameters
+    LMO_init = eps(FT)
+    u_star_init = FT(0.1)
+    th_star_init = -FT(0.1)
+    qt_star_init = -FT(1e-5)
+    MO_param_guess =
+        ArrayType(FT[LMO_init, u_star_init, th_star_init, qt_star_init])
+
+    # Surface values for variables
+    jj = 1
+    u_sfc = FT(0)
+    b_sfc = Tuple(ave(b))[jj]
+    qt_sfc = Tuple(ave(qt))[jj]
+    θ_sfc = Tuple(ave(θli))[jj]
+    z_sfc = FT(0)
+    x_s = ArrayType(FT[u_sfc, b_sfc, qt_sfc])
+
+    # Roughness
+    ## Roughness lengths
+    z0 = ArrayType(FT[
+        5.86144925739178e-05,
+        0.0001,
+        0.000641655193293549,
+        3.23383768877187e-05,
+    ])
+    zt = ArrayType(FT[
+        3.69403636275411e-05,
+        0.0001,
+        1.01735489109205e-05,
+        7.63933834969505e-05,
+    ])
+    zq = ArrayType(FT[
+        5.72575636226887e-05,
+        0.0001,
+        5.72575636226887e-05,
+        5.72575636226887e-05,
+    ])
+    z_rough = ArrayType(FT[Tuple(z0)[ii], Tuple(zt)[ii], Tuple(zq)[ii]])
+
+    # Constants
+    Δz = Tuple(z)[ii]
+
+    args = (
+        param_set,
+        MO_param_guess,
+        x_ave,
+        x_s,
+        z_rough,
+        θ_ave,
+        qt_ave,
+        z_ave / 2,
+    )
+
+    result = surface_conditions(args..., SF.DGScheme())
+    @show result
 end
