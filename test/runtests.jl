a77c68698c38849c2b44be91779ae213e875c582
Zhaoyi Shen <pkuszy@gmail.com>
2022-08-12 14:59:09 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -139,7 +139,7 @@ const sf_params = SurfaceFluxes.Parameters.SurfaceFluxesParameters{
             )
 
             sfc_output = SF.surface_conditions(sf_params, sc)
-            @test SF.obukhov_length(sfc_output) > FT(0)
+            @test abs(SF.obukhov_length(sfc_output)) > FT(0)
             @test sign(SF.non_zero(1.0)) == 1
             @test sign(SF.non_zero(-1.0)) == -1
             @test sign(SF.non_zero(-0.0)) == 1
