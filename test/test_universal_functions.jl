5550bf8b6a7449836d262271f320a224eaffb535
gdecker1 <gdecker@caltech.edu>
2023-06-28 18:09:49 -0700

    Added support for Beljaars (1991) UF
    * Implemented Beljaars phi and psi equations
    * Implemented Beljaars Psi equations
    * Added Beljaars to tests
    * Added Beljaars to plots
    * Updated type stability and convergence tests

--- test/test_universal_functions.jl
+++ test/test_universal_functions.jl
@@ -25,7 +25,7 @@ universal_functions(uft, L) = UF.universal_func(uft, L, create_uf_parameters(tom
         FT = Float32
         ζ = FT(-2):FT(0.01):FT(200)
         for L in (-FT(10), FT(10))
-            for uft in (UF.GryanikType(), UF.GrachevType(), UF.BusingerType(), UF.HoltslagType())
+            for uft in (UF.GryanikType(), UF.GrachevType(), UF.BusingerType(), UF.BeljaarsType(), UF.HoltslagType())
                 uf = universal_functions(uft, L)
                 for transport in (UF.MomentumTransport(), UF.HeatTransport())
                     ϕ = UF.phi.(uf, ζ, transport)
@@ -40,7 +40,7 @@ universal_functions(uft, L) = UF.universal_func(uft, L, create_uf_parameters(tom
         FT = Float32
         ζ = (-FT(1), FT(0.5) * eps(FT), 2 * eps(FT))
         for L in (-FT(10), FT(10))
-            for uft in (UF.GryanikType(), UF.GrachevType(), UF.BusingerType(), UF.HoltslagType())
+            for uft in (UF.GryanikType(), UF.GrachevType(), UF.BusingerType(), UF.BeljaarsType(), UF.HoltslagType())
                 uf = universal_functions(uft, L)
                 for transport in (UF.MomentumTransport(), UF.HeatTransport())
                     ϕ = UF.phi.(uf, ζ, transport)
@@ -55,7 +55,7 @@ universal_functions(uft, L) = UF.universal_func(uft, L, create_uf_parameters(tom
         FT = Float32
         ζ = (-FT(1), -FT(0.5) * eps(FT), FT(0.5) * eps(FT), 2 * eps(FT))
         for L in (-FT(10), FT(10))
-            for uft in (UF.GryanikType(), UF.BusingerType(), UF.HoltslagType())
+            for uft in (UF.GryanikType(), UF.BusingerType(), UF.BeljaarsType(), UF.HoltslagType())
                 uf = universal_functions(uft, L)
                 for transport in (UF.MomentumTransport(), UF.HeatTransport())
                     Ψ = UF.Psi.(uf, ζ, transport)
