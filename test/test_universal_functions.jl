530179a0abdc2a0407f1a289aa39d222a37a7008
Akshay Sridhar <asridhar@clima.gps.caltech.edu>
2025-10-30 10:08:46 -0700

    Simplify universal function type structure 	modified:   src/Parameters.jl 	modified:   src/SurfaceFluxes.jl 	modified:   src/UniversalFunctions.jl 	modified:   test/runtests.jl 	modified:   test/test_convergence.jl 	modified:   test/test_profiles.jl 	modified:   test/test_universal_functions.jl

--- test/test_universal_functions.jl
+++ test/test_universal_functions.jl
@@ -5,9 +5,10 @@ import QuadGK
 import SurfaceFluxes as SF
 import SurfaceFluxes.UniversalFunctions as UF
 import ClimaParams as CP
+import SurfaceFluxes.Parameters as SFP
 
 FT = Float32
-param_set = SFP.SurfaceFluxesParameters(FT, BusingerParams)
+param_set = SFP.SurfaceFluxesParameters(FT, UF.BusingerParams)
 thermo_params = param_set.thermo_params
 
 # TODO: Right now, we test these functions for
@@ -18,54 +19,42 @@ thermo_params = param_set.thermo_params
     @testset "Type stability" begin
         FT = Float32
         ζ = FT(-2):FT(0.01):FT(200)
-        for L in (-FT(10), FT(10))
-            for ufp in (
-                UF.GryanikParams(FT),
-                UF.GrachevParams(FT),
-                UF.BusingerParams(FT),
-            )
-                uft = UF.universal_func_type(typeof(ufp))
-                uf = UF.universal_func(uft, L, ufp)
-                for transport in (UF.MomentumTransport(), UF.HeatTransport())
-                    ϕ = UF.phi.(uf, ζ, transport)
-                    @test eltype(ϕ) == FT
-                    ψ = UF.psi.(uf, ζ, transport)
-                    @test eltype(ψ) == FT
-                end
+        for ufp in (
+            UF.GryanikParams(FT),
+            UF.GrachevParams(FT),
+            UF.BusingerParams(FT),
+        )
+            for transport in (UF.MomentumTransport(), UF.HeatTransport())
+                ϕ = UF.phi.(ufp, ζ, transport)
+                @test eltype(ϕ) == FT
+                ψ = UF.psi.(ufp, ζ, transport)
+                @test eltype(ψ) == FT
             end
         end
 
         # More type stability (phi/psi):
         FT = Float32
         ζ = (-FT(1), FT(0.5) * eps(FT), 2 * eps(FT))
-        for L in (-FT(10), FT(10))
-            for ufp in (
-                UF.GryanikParams(FT),
-                UF.GrachevParams(FT),
-                UF.BusingerParams(FT),
-            )
-                uft = UF.universal_func_type(typeof(ufp))
-                uf = UF.universal_func(uft, L, ufp)
-                for transport in (UF.MomentumTransport(), UF.HeatTransport())
-                    ϕ = UF.phi.(uf, ζ, transport)
-                    @test eltype(ϕ) == FT
-                    ψ = UF.psi.(uf, ζ, transport)
-                    @test eltype(ψ) == FT
-                end
+        for ufp in (
+            UF.GryanikParams(FT),
+            UF.GrachevParams(FT),
+            UF.BusingerParams(FT),
+        )
+            for transport in (UF.MomentumTransport(), UF.HeatTransport())
+                ϕ = UF.phi.(ufp, ζ, transport)
+                @test eltype(ϕ) == FT
+                ψ = UF.psi.(ufp, ζ, transport)
+                @test eltype(ψ) == FT
             end
         end
 
         # More type stability (Psi):
         FT = Float32
         ζ = (-FT(1), -FT(0.5) * eps(FT), FT(0.5) * eps(FT), 2 * eps(FT))
-        for L in (-FT(10), FT(10))
-            for ufp in (UF.GryanikParams(FT), UF.BusingerParams(FT))
-                uft = UF.universal_func_type(typeof(ufp))
-                uf = UF.universal_func(uft, L, ufp)
-                for transport in (UF.MomentumTransport(), UF.HeatTransport())
-                    Ψ = UF.Psi.(uf, ζ, transport)
-                    @test eltype(Ψ) == FT
-                end
+        for ufp in (UF.GryanikParams(FT), UF.BusingerParams(FT))
+            for transport in (UF.MomentumTransport(), UF.HeatTransport())
+                Ψ = UF.Psi.(ufp, ζ, transport)
+                @test eltype(Ψ) == FT
             end
         end
 
@@ -73,30 +62,23 @@ thermo_params = param_set.thermo_params
     @testset "Asymptotic range" begin
         FT = Float32
 
-        ϕ_h_ζ∞(uf::UF.Grachev, ζ) = 1 + FT(UF.b_h(uf))
-        ϕ_m_ζ∞(uf::UF.Grachev, ζ) =
-            FT(UF.a_m(uf)) / FT(UF.b_m(uf)) * ζ^FT(1 / 3)
+        ϕ_h_ζ∞(p::UF.GrachevParams, ζ) = 1 + FT(p.b_h)
+        ϕ_m_ζ∞(p::UF.GrachevParams, ζ) = FT(p.a_m) / FT(p.b_m) * ζ^FT(1 / 3)
 
-        ϕ_h_ζ∞(uf::UF.Gryanik, ζ) =
-            FT(1) +
-            (FT(ζ) * FT(UF.Pr_0(uf)) * FT(UF.a_h(uf))) /
-            (1 + FT(UF.b_h(uf)) * FT(ζ))
-        ϕ_m_ζ∞(uf::UF.Gryanik, ζ) =
-            FT(UF.a_m(uf) / UF.b_m(uf)^FT(2 / 3)) * ζ^FT(1 / 3)
+        ϕ_h_ζ∞(p::UF.GryanikParams, ζ) =
+            FT(1) + (FT(ζ) * FT(p.Pr_0) * FT(p.a_h)) / (1 + FT(p.b_h) * FT(ζ))
+        ϕ_m_ζ∞(p::UF.GryanikParams, ζ) =
+            FT(p.a_m / p.b_m^FT(2 / 3)) * ζ^FT(1 / 3)
 
 
-        for L in (-FT(10), FT(10))
-            for ufp in (UF.GryanikParams(FT), UF.GrachevParams(FT))
-                uft = UF.universal_func_type(typeof(ufp))
-                uf = UF.universal_func(uft, L, ufp)
-                for ζ in FT(10) .^ (4, 6, 8, 10)
-                    ϕ_h = UF.phi(uf, ζ, UF.HeatTransport())
-                    @test isapprox(ϕ_h, ϕ_h_ζ∞(uf, ζ))
-                end
-                for ζ in FT(10) .^ (8, 9, 10)
-                    ϕ_m = UF.phi(uf, ζ, UF.MomentumTransport())
-                    @test isapprox(ϕ_m, ϕ_m_ζ∞(uf, ζ))
-                end
+        for ufp in (UF.GryanikParams(FT), UF.GrachevParams(FT))
+            for ζ in FT(10) .^ (4, 6, 8, 10)
+                ϕ_h = UF.phi(ufp, ζ, UF.HeatTransport())
+                @test isapprox(ϕ_h, ϕ_h_ζ∞(ufp, ζ))
+            end
+            for ζ in FT(10) .^ (8, 9, 10)
+                ϕ_m = UF.phi(ufp, ζ, UF.MomentumTransport())
+                @test isapprox(ϕ_m, ϕ_m_ζ∞(ufp, ζ))
             end
         end
 
@@ -105,14 +87,10 @@ thermo_params = param_set.thermo_params
     # Test for Gryanik2021 Eq. 2 & 3; ensures Ψ(0) = 0
     @testset "Vanishes at Zero" begin
         FT = Float32
-        for L in (-FT(10), FT(10))
-            for ufp in (UF.GryanikParams(FT), UF.GrachevParams(FT))
-                for transport in (UF.HeatTransport(), UF.MomentumTransport())
-                    uft = UF.universal_func_type(typeof(ufp))
-                    uf = UF.universal_func(uft, L, ufp)
-                    Ψ_0 = UF.psi(uf, FT(0), transport)
-                    @test isapprox(Ψ_0, FT(0))
-                end
+        for ufp in (UF.GryanikParams(FT), UF.GrachevParams(FT))
+            for transport in (UF.HeatTransport(), UF.MomentumTransport())
+                Ψ_0 = UF.psi(ufp, FT(0), transport)
+                @test isapprox(Ψ_0, FT(0))
             end
         end
     end
@@ -132,32 +110,24 @@ thermo_params = param_set.thermo_params
                 FT(10),
                 FT(20),
             )
-            for L in FT(10) .* sign.(ζ_array)
-                for ζ in ζ_array
-                    for ufp in (
-                        UF.GryanikParams(FT),
-                        UF.GrachevParams(FT),
-                        UF.BusingerParams(FT),
-                    )
-                        uft = UF.universal_func_type(typeof(ufp))
-                        uf = UF.universal_func(uft, L, ufp)
-                        for transport in
-                            (UF.MomentumTransport(), UF.HeatTransport())
-                            # Compute ψ via numerical integration of 𝒻(ϕ(ζ))
-                            ψ_int = QuadGK.quadgk(
-                                ζ′ ->
-                                    (FT(1) - UF.phi(uf, ζ′, transport)) / ζ′,
-                                eps(FT),
-                                ζ,
-                            )
-                            # Compute ψ using function definitions of ψ(ζ)
-                            ψ = UF.psi(uf, ζ, transport)
-                            @test isapprox(
-                                ψ_int[1] - ψ,
-                                FT(0),
-                                atol = 10sqrt(eps(FT)),
-                            )
-                        end
+            for ζ in ζ_array
+                for ufp in (
+                    UF.GryanikParams(FT),
+                    UF.GrachevParams(FT),
+                    UF.BusingerParams(FT),
+                )
+                    for transport in (UF.MomentumTransport(), UF.HeatTransport())
+                        ψ_int = QuadGK.quadgk(
+                            ζ′ -> (FT(1) - UF.phi(ufp, ζ′, transport)) / ζ′,
+                            eps(FT),
+                            ζ,
+                        )
+                        ψ = UF.psi(ufp, ζ, transport)
+                        @test isapprox(
+                            ψ_int[1] - ψ,
+                            FT(0),
+                            atol = 10sqrt(eps(FT)),
+                        )
                     end
                 end
             end
